67f04fce5a3fd9616eebd0d70751660d06584f23
杨旭 <xu.yang@ishansong.com>
2018-03-23 14:18:29 +0800

    Initial commit

---
+++ app/src/main/java/com/yx/sreader/activity/WelcomeActivity.java
@@ -0,0 +1,10 @@
+package com.yx.sreader.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iss on 2018/3/23.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+}
